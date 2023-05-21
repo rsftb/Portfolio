a501492069ff594ae065a8622697f4cff59bebd7
RS <132225296+rsftb@users.noreply.github.com>
2023-05-21 18:02:03 +0200

    Add files via upload

---
+++ C++/Calculator/headers/calculator.h
@@ -0,0 +1,8 @@
+
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+int addition();
+int subtraction();
+
+#endif
---
+++ C++/Calculator/main.cpp
@@ -0,0 +1,23 @@
+
+#include "headers/common.h"
+#include "headers/calculator.h"
+
+/*
+\ Simple calculator that has type converted output.
+\ Keeps looping until you type "no" when it asks you to play again
+\
+*/
+
+int main(){
+
+    cout << "Addition" << endl;
+    addition();
+    cout << endl;
+
+    cout << "Subtraction" << endl;
+    subtraction();
+    cout << endl;
+
+    return 0;
+
+}
